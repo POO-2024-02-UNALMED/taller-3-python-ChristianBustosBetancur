b5a079caa4a9224075ee11f4ab6757ff15554dc1
Christian Bustos Betancur <111025495+ChristianBustosBetancur@users.noreply.github.com>
2024-11-15 21:20:49 +0000

    Prueba1.8

--- televisores/control.py
+++ televisores/control.py
@@ -18,9 +18,11 @@ class Control:
         self._tv.volumenDown()    
 
     def setCanal(self,canal):
-        self._canal = canal
+        if self._tv == True and 1 < canal <= 120:
+            self._canal = canal
     def setVolumen(self,volumen):
-        self._volumen = volumen  
+        if self._tv == True and 1 < volumen <= 120:
+            self._volumen = volumen   
         
     def enlazar(self, tv):
         self._tv = tv
